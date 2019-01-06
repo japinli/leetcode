68d0c4d1404f384a507addd91aba7c6bbbf01e10
japinli <japinli@hotmail.com>
2019-01-06 22:02:13 +0800

    Remove Shell subdirtory

--- Shell/Tenth.Line/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -n '10p' file.txt
-
-awk 'NF == 10' file.txt
-
-tail -n+10 file.txt | head -1
-
--- Shell/Valid.Phone.Numbers/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Solution 1
-sed -nr '/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/p' file.txt
-
-# Solution 2 - use perl regex expression
-grep -P '^(\d{3}-|\(\d{3}\) )\d{3}-\d{4}$' file.txt
